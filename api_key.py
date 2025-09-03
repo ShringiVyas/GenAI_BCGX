af09dd2bc570add7863b1352d2da861e941de23f
Shringi Vyas <118166602+ShringiVyas@users.noreply.github.com>
2025-09-03 15:52:15 +0530

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -1,2 +1,4 @@
-llama_key = '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'
+# llama_key = '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'
+llama_key = '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'
+
 
